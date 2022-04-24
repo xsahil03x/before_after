8b4a727e8a4aaee13b5e0fbf6ff4634215dbac61
yuran09 <yuranfgs@gmail.com>
2022-04-25 00:31:13 +0200

    - zoom and pan image - switch when to zoom/slide by longpressing thumb - Note: thumb also gets scaled when scaling image (couldn't find a way to make it static) - there's a delay when switching to zoom/slide

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/andrey/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/andrey/FlutterProjects/before_after/example"
+export "FLUTTER_ROOT=C:\Users\yuran\Documents\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\yuran\AndroidStudioProjects\before_after\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
